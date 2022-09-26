c370990c7f8f19ed175ad80b81bab027f7ba4cdd
irfan <irfansultan201@gmail.com>
2022-09-26 11:36:46 +0500

    link change

--- singupserver.mjs
+++ singupserver.mjs
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000', "*"],
+    origin: ['https://weatherappreact2588.netlify.app/login', "*"],
     credentials: true
 }));
 
